d2be6bc792fb24b5813b6a19899d08b99439349f
jumborami <jumborami@gmail.com>
2020-12-12 11:50:33 +0900

    sequelize이어서

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.listen(3000, () => {
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 const sequelize = new Sequelize({ //create pool
   host: 'localhost',
@@ -41,9 +44,24 @@ const Sample = sequelize.define('Sample', {
 
 sequelize.sync();
 
-app.get('/', (req, res, next) => {
+/* app.get('/', (req, res, next) => {
   res.send('<h1>Hello Sequelize</h1>');
+}); */
+
+app.get('/write', (req, res, next) => {
+  res.render('write.pug');
 });
+
+app.post('/save', async (req, res, next) => {
+  let { title, writer, comment } = req.body;
+  try {
+    let result = await Sample.create({title, writer, comment});//혹은 let 선언한거 지우고 ...req.body 만 여기 넣어도 됨
+    res.redirect('/list');
+  }
+  catch (e) {
+    console.log(e);
+  }
+})
 
 app.get('/create', async (req, res, next) => {
   try {
@@ -59,17 +77,36 @@ app.get('/create', async (req, res, next) => {
   }
 });
 
-app.get('/list', async (req, res, next) => {
+app.get(['/', '/list'], async (req, res, next) => {
   try {
     let result = await Sample.findAll({ // /list로 요청 들어오면 데이터 모두 가져오기
       order: [['id', 'desc']],
-      offset: 0, //0번부터
-      limit: 5 //5개 가져오기 //보통 sql문이라면? limit: (0, 5) 라고 씀
+      //offset: 0, //0번부터
+      //limit: 5 //5개 가져오기 //보통 sql문이라면? limit: (0, 5) 라고 씀
     });
-    res.json(result);
+    res.render('list', { result });
   }
   catch(e) {
     console.log(e);
   }
-})
+});
 
+app.get('/update/:id', async (req, res, next) => {
+  let id = req.params.id
+  let v = await Sample.findOne({ //하나만 가져오기
+    where: { id } //'id': id
+  });
+  res.render('update.pug', { v });
+});
+
+app.post('/saveUpdate', async (req, res, next) => {
+  let { id, title, comment, writer } = req.body;
+  let result = await Sample.update({ title, comment, writer }, { where: { id } });
+  res.redirect('/');
+});
+
+app.get('/remove/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let result = await Sample.destroy({ where: { id } }); // 삭제
+  res.redirect('/');
+});
